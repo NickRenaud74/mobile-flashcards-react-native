dbec9edd12687f9e2c602fe332d7e81246ecee21
NickRenaud74 <nick_renaud@hotmail.com>
2021-03-08 08:58:10 -0800

    create redux store and use thunk middleware

--- App.js
+++ App.js
@@ -1,14 +1,23 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
+import { Provider } from 'react-redux'
+import { createStore, applyMiddleware } from 'redux'
+import thunk from 'redux-thunk'
+import reducer from './src/reducers'
 import { StyleSheet, Text, View } from 'react-native';
+
+
+const store = createStore(reducer, applyMiddleware(thunk))
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>MOBILE FLASHCARDS!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    <Provider store={store}>
+      <View style={styles.container}>
+        <Text>MOBILE FLASHCARDS!</Text>
+        <StatusBar style="auto" />
+      </View>
+    </Provider>
+  )
 }
 
 const styles = StyleSheet.create({
