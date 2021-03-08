a433ec8ce306c89714cb74a2ac94ae7381d76fd1
NickRenaud74 <nick_renaud@hotmail.com>
2021-03-07 19:00:39 -0800

    feat: add reducers for actions
    - reducers to update state of card decks
    - get decks retrieves all decks saved in async storage
    - remove deck deletes deck from state
    - add deck when creating a new deck
    - add card to add a question/answer to a certain card deck

---
+++ src/reducers/index.js
@@ -0,0 +1,33 @@
+import { GET_DECKS, REMOVE_DECK, ADD_DECK, ADD_CARD } from '../actions'
+
+export default function decks(state = {}, action ) {
+    switch(action.type) {
+        case GET_DECKS:
+            return {
+                ...state,
+                ...action.decks
+            }
+        case REMOVE_DECK:
+            const newDeck = Object.keys(state).filter(deck => deck !== action.deck)
+            return newDeck
+            
+        case ADD_DECK:
+            const title = action.deck
+            return {
+                ...state,
+                [title]: {
+                    title,
+                    questions: []
+                }
+            }
+        case ADD_CARD:
+            const { deck, card } = action
+            return {
+                ...state,
+                [deck]: {
+                    ...state[deck],
+                    questions: [...state[deck].questions, card]
+                }
+            }
+    }
+}
