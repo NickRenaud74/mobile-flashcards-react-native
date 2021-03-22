0873535dc07645bf2c41bc6b719ef673e4e00de0
NickRenaud74 <nick_renaud@hotmail.com>
2021-03-22 11:09:19 -0700

    cleanup imports

--- src/components/Deck.js
+++ src/components/Deck.js
@@ -1,4 +1,4 @@
-import React, { useLayoutEffect } from 'react'
+import React from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { Text, View, StyleSheet } from 'react-native'
 import { Button } from 'react-native-elements'
