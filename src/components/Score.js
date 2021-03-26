a2890a296de0d8a46fd4511db376a64ea96d82d6
NickRenaud74 <nick_renaud@hotmail.com>
2021-03-26 11:48:11 -0700

    feat: add restart quiz functionality
    -add restart quiz button which takes in the reset function from the quiz view and will reset all state and then render back to quiz component
    
    -add and style a percentage progress circle to display users score

--- src/components/Score.js
+++ src/components/Score.js
@@ -1,12 +1,38 @@
 import React from 'react'
 import { View, Text, StyleSheet } from 'react-native'
+import ProgressCircle from 'react-native-progress-circle'
+import CustomButton from './CustomButton'
+import { blue } from '../utils/colors'
+import Quiz from './Quiz'
 
-function Score({score, questions, reset}) {
+function Score({ score, questions, reset }) {
     const percentage = Math.round(score / questions * 100)
+
+    function restart() {
+        reset()
+       return <Quiz />
+    }
+
     return (
         <View style={styles.container}>
-            <Text> You scored {score} out of {questions}</Text>
-            <Text>{percentage} %</Text>
+            <Text style={styles.scoreText}> You scored {score} out of {questions}</Text>
+            <ProgressCircle
+                percent={percentage}
+                radius={90}
+                borderWidth={10}
+                color={blue}
+                shadowColor="#999"
+                bgColor="#fff"
+                outerCircleStyle={{margin: 45}}
+
+            >
+                <Text style={styles.percentText}>{`${percentage} %`}</Text>
+            </ProgressCircle>
+            <CustomButton 
+                title='Restart Quiz'
+                color={blue}
+                onPress={restart}
+            />
         </View>
     )
 }
@@ -16,6 +42,14 @@ const styles = StyleSheet.create({
         flex: 1,
         margin: 50
     },
+    scoreText: {
+        fontSize: 24,
+    },
+    percentText: {
+        fontSize: 24,
+        fontWeight: 'bold',
+        color: blue
+    }
 })
 
 export default Score
